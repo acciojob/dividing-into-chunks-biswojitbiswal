9dd69eb4ce736f9f80e82bd7b43618e55bcdcabf
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-20 10:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	if (sum + arr[i] > n) {
+
+      arrList.push(currentList);
+      currentList = [];
+      sum = 0;
+    }
+	
+    currentList.push(arr[i]);
+    sum += arr[i];
+  }
+
+
+  if (currentList.length > 0) {
+    arrList.push(currentList);
+  }
+
+  return arrList;
 };
 
-const n = prompt("Enter n: ");
+const n = prompt("Enter n: "); 
 alert(JSON.stringify(divide(arr, n)));
